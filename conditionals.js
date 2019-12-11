98350bd043394f13aa991c320033faa490354096
Ahmed Alwardani <aalwa049@uottawa.ca>
2019-12-11 01:45:24 +0000

    December 10 updates

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,60 @@
-const temperature = 12;
+// const raining = true;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// const raining = true;
+// const cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// const cold = false;
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+
+// const temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
 
-console.log("Now you're ready to go outside!");
+temperature = -45;
+
+if(temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't suach a great idea...");
+}
+
+raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+// December 10 comments
